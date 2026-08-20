7e62dcec8d6759f6c5f9adf40799dadbcccb56b7
Thierry Crouzet <tc@tcrouzet.com>
2026-08-20 17:37:40 +0200

    sync 2026-08-20 17:37:40

--- web.sh
+++ web.sh
@@ -5,3 +5,5 @@ cd "$PROJECT_ROOT"
 PYTHON="$PROJECT_ROOT/venv/bin/python"
 if [ ! -x "$PYTHON" ]; then PYTHON=python3; fi
 PYTHONPATH=script "$PYTHON" -m detector.web_export
+VERSION=$(date +%Y%m%d%H%M%S)
+VERSION="$VERSION" perl -pi -e 's/__WEB_VERSION__/$ENV{VERSION}/g; s/(style\.css\?v=)[^"]+/${1}$ENV{VERSION}/g; s/(app\.js\?v=)[^"]+/${1}$ENV{VERSION}/g; s/(favicon\.svg\?v=)[^"]+/${1}$ENV{VERSION}/g; s/(social-preview\.png\?v=)[^"]+/${1}$ENV{VERSION}/g' web/index.html
