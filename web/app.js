15a390a0125317bcf84b1e10c43d24a75878f9fb
Thierry Crouzet <tc@tcrouzet.com>
2026-08-22 16:53:18 +0200

    sync 2026-08-22 16:53:18

--- web/app.js
+++ web/app.js
@@ -444,7 +444,7 @@ function controls() {
   authorLimitsButton.addEventListener("click", () => { corpusProfile = true; authorProfile = false; authorLimits = true; draw(); });
   worksButton.addEventListener("click", () => { authorProfile = false; corpusProfile = false; authorLimits = false; showWorksMode(); draw(); });
 }
-fetch("data.json?v=20260822155917120527000").then(r => r.json()).then(json => {
+fetch("data.json?v=20260822162415413926000").then(r => r.json()).then(json => {
   data = json;
   COLORS = Object.entries(data.palette || {}).filter(([key, color]) => key.startsWith("color") && color).map(([, color]) => color);
   IA_COLOR = data.palette?.ia || IA_COLOR;
