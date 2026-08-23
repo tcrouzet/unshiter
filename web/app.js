6547a5a74ba858ef33b9982ca968c54249ebab07
Thierry Crouzet <tc@tcrouzet.com>
2026-08-23 18:44:41 +0200

    sync 2026-08-23 18:44:41

--- web/app.js
+++ web/app.js
@@ -872,7 +872,7 @@ function controls() {
   authorLimitsButton.addEventListener("click", () => { corpusProfile = true; authorProfile = false; authorLimits = true; localStorage.setItem("unshiter-view-mode", "author-limits"); draw(); });
   worksButton.addEventListener("click", () => { authorProfile = false; corpusProfile = false; authorLimits = false; localStorage.setItem("unshiter-view-mode", "works"); showWorksMode(); draw(); });
 }
-fetch("data.json?v=20260823152125230308000").then(r => r.json()).then(json => {
+fetch("data.json?v=20260823184228043284000").then(r => r.json()).then(json => {
   data = json;
   COLORS = Object.entries(data.palette || {}).filter(([key, color]) => key.startsWith("color") && color).map(([, color]) => color);
   IA_COLOR = data.palette?.ia || IA_COLOR;
